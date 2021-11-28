ea9c563880c5bc0e46e4f901687f911e1bf20e88
Bombozaur666 <bombozaur6@gmail.com>
2021-11-29 00:28:23 +0100

    -added bootstrap

--- learningDjango/settings.py
+++ learningDjango/settings.py
@@ -124,8 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'bootstrap'),
 )
 
 # Default primary key field type
